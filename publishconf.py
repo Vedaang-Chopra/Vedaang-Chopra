6b7df296714b44ff7eeca95c27c2ccc7e1186c1c
Vedaang Chopra <vedaangchopra@gatech.edu>
2025-12-12 04:47:22 -0500

    Update SITEURL to custom domain to fix CSS paths

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://vedaang-chopra.github.io/Vedaang-Chopra'
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://vedaangchopra.live'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
